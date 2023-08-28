f8233cb253e5913d16da0a1fcd387968761495c4
Toan Thang <box.dota@gmail.com>
2023-08-28 04:14:36 +0300

    Spawning Tiles

--- Assets/Scripts/TileBoard.cs
+++ Assets/Scripts/TileBoard.cs
@@ -1,10 +1,49 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class TileBoard : MonoBehaviour
 {
     public Tile tilePrefabs;
+    public TileState[] tileStates;
+
     private TileGrid grid;
     private List<Tile> tiles;
 
+    private void Awake()
+    {
+        grid = GetComponentInChildren<TileGrid>();
+        tiles = new List<Tile>(16);
+    }
+
+    private void Start()
+    {
+        CreateTile();
+        CreateTile();
+    }
+
+    private void CreateTile()
+    {
+        Tile tile = Instantiate(tilePrefabs, grid.transform);
+        tile.SetState(tileStates[0]);
+        tile.Spawn(grid.GetRandomEmptyCell());
+        tiles.Add(tile);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+        }
+    }
 }
