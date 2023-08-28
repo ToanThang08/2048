0a8db52b0d93be2a36f8afaadabad0febfc8a4de
Toan Thang <box.dota@gmail.com>
2023-08-28 03:40:42 +0300

    Creating Tile, TileBroad, TileCell, TileGrid, TileRow, TileState

---
+++ Assets/Scripts/TileBoard.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBoard : MonoBehaviour
+{
+    public Tile tilePrefabs;
+    private TileGrid grid;
+    private List<Tile> tiles;
+
+}
